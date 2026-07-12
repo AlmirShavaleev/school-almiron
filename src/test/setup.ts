d616b7f07fce97415ece3821fda6072dfe54742f
almirshavaleev-collab <almir.shavaleev@lycee1.ru>
2026-07-12 23:41:48 +0300

    test: mock canvas in jsdom

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,48 @@
 import '@testing-library/jest-dom'
+
+const mockCanvasContext = {
+  canvas: null as HTMLCanvasElement | null,
+  clearRect: () => {},
+  save: () => {},
+  restore: () => {},
+  scale: () => {},
+  rotate: () => {},
+  translate: () => {},
+  transform: () => {},
+  setTransform: () => {},
+  resetTransform: () => {},
+  beginPath: () => {},
+  closePath: () => {},
+  moveTo: () => {},
+  lineTo: () => {},
+  stroke: () => {},
+  fill: () => {},
+  rect: () => {},
+  fillRect: () => {},
+  strokeRect: () => {},
+  clip: () => {},
+  arc: () => {},
+  arcTo: () => {},
+  ellipse: () => {},
+  bezierCurveTo: () => {},
+  quadraticCurveTo: () => {},
+  drawImage: () => {},
+  createImageData: () => ({ data: new Uint8ClampedArray(0) }),
+  getImageData: () => ({ data: new Uint8ClampedArray(0) }),
+  putImageData: () => {},
+  createLinearGradient: () => ({ addColorStop: () => {} }),
+  createPattern: () => null,
+  createRadialGradient: () => ({ addColorStop: () => {} }),
+  fillText: () => {},
+  strokeText: () => {},
+  measureText: () => ({ width: 0 }),
+  setLineDash: () => {},
+  getLineDash: () => [],
+} satisfies Partial<CanvasRenderingContext2D>
+
+Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
+  configurable: true,
+  value: function getContext() {
+    return { ...mockCanvasContext, canvas: this }
+  },
+})
